24b6926bcbc5da9448e4637e42eb6db07cd2d026
lamb-liver <147922854+lamb-liver@users.noreply.github.com>
2026-05-12 03:19:34 +0800

    優化顯示問題

---
+++ public/sw.js
@@ -0,0 +1,140 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-b1bafff1'], (function (workbox) { 'use strict';
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "index.html",
+    "revision": "c91184fdb7b3ea283d4990c10fb5ce92"
+  }, {
+    "url": "icons.svg",
+    "revision": "3b4fcfcf393eca4d264dca4a4663bc37"
+  }, {
+    "url": "favicon.svg",
+    "revision": "7e840862161341271697daa99a40d76b"
+  }, {
+    "url": "assets/vendor-react-core-lmBx3MHZ.js",
+    "revision": null
+  }, {
+    "url": "assets/vendor-html2canvas-RmJPy6_o.js",
+    "revision": null
+  }, {
+    "url": "assets/vendor-common-BZFbPMOR.js",
+    "revision": null
+  }, {
+    "url": "assets/rolldown-runtime-S-ySWqyJ.js",
+    "revision": null
+  }, {
+    "url": "assets/index-DYZRW8-Y.css",
+    "revision": null
+  }, {
+    "url": "assets/index-BIPFEYOt.js",
+    "revision": null
+  }, {
+    "url": "favicon.svg",
+    "revision": "7e840862161341271697daa99a40d76b"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "8aeb48488a0d7a93c8c2964c02e92965"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
+    denylist: [/^\/api\//]
+  }));
+  workbox.registerRoute(({
+    url
+  }) => /\.webp$/i.test(url.pathname), new workbox.CacheFirst({
+    "cacheName": "accusation-webp",
+    plugins: [new workbox.ExpirationPlugin({
+      maxEntries: 800,
+      maxAgeSeconds: 31536000
+    }), new workbox.CacheableResponsePlugin({
+      statuses: [0, 200]
+    })]
+  }), 'GET');
+  workbox.registerRoute(({
+    url
+  }) => /\/assets\/.*\.(?:js|mjs|css)$/i.test(url.pathname), new workbox.StaleWhileRevalidate({
+    "cacheName": "accusation-bundled-assets",
+    plugins: [new workbox.ExpirationPlugin({
+      maxEntries: 64,
+      maxAgeSeconds: 604800
+    }), new workbox.CacheableResponsePlugin({
+      statuses: [0, 200]
+    })]
+  }), 'GET');
+
+}));
